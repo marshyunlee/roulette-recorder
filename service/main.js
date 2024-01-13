37d8677f76514f08eb2b0e08ec0d11dd56750355
Marshall <gkslql456@gmail.com>
2024-01-12 22:30:38 -0800

    added main script

---
+++ service/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+function createWindow () {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 400,
+        height: 500,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
